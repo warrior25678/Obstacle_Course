1cf89d9856742e1d246984fddb4ca9d02ad484b9
Matthew Lonar <matthewlonar@gmail.com>
2023-05-11 18:37:19 -0300

    Added spinner object.

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float spinSpeed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, spinSpeed, 0);
+    }
+}
