87228cec816f92f4df104d3674341b2fa0671375
Matthew Lonar <matthewlonar@gmail.com>
2023-05-11 18:14:31 -0300

    Added Player tag to player_sphere. Updated code so obstacles only react if something with the Player tag hits them.

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -6,6 +6,10 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.magenta;
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.magenta;
+        }
+
     }
 }
